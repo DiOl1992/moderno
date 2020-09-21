7f773e58ec94b4e7604b00bbe526c6cb564903ef
Diana <dianagerasimova1992@gmail.com>
2020-09-21 14:40:39 +0300

    added settings page

--- app/js/main.js
+++ app/js/main.js
@@ -42,10 +42,10 @@ $(function () {
     $('.header__box').toggleClass('active');
   });
 
-  $('.product-one-tabs .tab').on('click', function (event) {
+  $('.product-one-tabs .tab, .settings__tabs .tab').on('click', function (event) {
     var id = $(this).attr('data-id');
-    $('.product-one-tabs').find('.tab-item').removeClass('active-tab').hide();
-    $('.product-one-tabs .tabs').find('.tab').removeClass('active');
+    $('.product-one-tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one-tabs .tabs, .settings__tabs .tab').find('.tab').removeClass('active');
     $(this).addClass('active');
     $('#' + id).addClass('active-tab').fadeIn();
     return false;
